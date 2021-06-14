4961887f19cf2a2f20f51d2c895f647028941743
Minh Dinh Nguyen <minh.nguyen2@hitachivantara.com>
2021-06-14 19:27:07 +0700

    done:
    - Firebase connection
    - React Context for Firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { FireBaseContext } from './context/firebase';
 import { GlobalStyles } from './global-styles';
+import { firebase } from './lib/firebase.prod';
 
 render(
     <>
-        <GlobalStyles />
-        <App />
+        <FireBaseContext.Provider value={{firebase}}>
+            <GlobalStyles />
+            <App />
+        </FireBaseContext.Provider>
     </>,
     document.getElementById('root'));
 
