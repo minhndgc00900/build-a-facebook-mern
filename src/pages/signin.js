985ce450bec1332d15ddb5eee37fe93eeeb3df11
Minh Dinh Nguyen <minh.nguyen2@hitachivantara.com>
2021-06-16 23:29:31 +0700

    DONE:
    03:10:16 - Firebase connection
    03:24:14 - React Context for Firebase
    03:28:56 - SignIn Page
    03:54:52 - Firebase top level authentication
    03:58:56 - Firebase console authentication
    04:00:25 - Debugging error auth (if user is in the db or not)
    04:01:51 - SignUp Page
    04:09:22 - Registration page, routes debugging

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -59,7 +59,7 @@ function SignIn () {
                         </Form.Submit>
 
                         <Form.Text>
-                            New to Netflix? <Form.Link to='/signup'>Sign up now.</Form.Link>
+                            New to Netflix? <Form.Link to='/sign-up'>Sign up now.</Form.Link>
                         </Form.Text>
                         <Form.TextSmall>
                             This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,10 +1,93 @@
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+import React, {
+  useContext,
+  useState
+} from 'react'
+import {
+  useHistory
+} from 'react-router-dom'
+import { HeaderContainer } from '../containers/header'
+import { Form } from '../components'
+import {
+  FireBaseContext
+} from '../context/firebase'
+import * as ROUTES from '../constants/routes'
 
 function SignUp () {
+  const history = useHistory()
+  const {
+    firebase
+  } = useContext(FireBaseContext)
+
+  const [firstName, setFirstName] = useState('')
+  const [emailAddress, setEmailAddress] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const isInvalid = firstName === '' || password === '' ||
+    emailAddress === ''
+
+  const handleSignUp = event => {
+    event.preventDefault()
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE)
+          })
+      )
+      .catch((error) => {
+        setFirstName('')
+        setEmailAddress('')
+        setPassword('')
+        setError(error.message)
+      })
+  }
+
   return (
-        <div>
-            Hello from sign up
-        </div>
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignUp} method='POST'>
+            <Form.Input
+              placeholder="First name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              value={password}
+              autoComplete='off'
+              type='password'
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type='submit' data-testid='sign-up'>
+              Sign up
+            </Form.Submit>
+            <Form.Text>
+              Already a user? <Form.Link to='/sign-in'>Sign in now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+    </>
   )
 }
 
