d4358ce55493574492fa52d012f9d711581c165a
Minh Dinh Nguyen <minh.nguyen2@hitachivantara.com>
2021-06-17 19:53:03 +0700

    in progress router authentication

--- src/App.js
+++ src/App.js
@@ -11,12 +11,29 @@ import {
   SignIn,
   SignUp
 } from './pages'
+import { IsUserRedirect } from './helpers/routes';
 
 function App () {
+  const user = {};
+
+  const isUserRedirect = (children, loggedInPath) => {
+    !user ? children : (
+      <Redirect to={{
+          pathname: loggedInPath,
+      }} />
+  )
+  }
+
   return (
     <Router>
-      <Route exact path={ROUTES.HOME} component={Home} />
-      <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+      <Route exact 
+        path={ROUTES.HOME} 
+        component={Home} 
+        render={isUserRedirect(ROUTES.SIGN_IN ,ROUTES.HOME)}
+      />
+      <Route 
+        path={ROUTES.SIGN_IN} 
+        component={SignIn}  />
       <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
       <Route exact path={ROUTES.BROWSE} component={Browse} />
     </Router>
---
+++ src/helpers/routes.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function IsUserRedirect(props) {
+    const { user, loggedInPath, children, ...rest} = props;
+    const checkUserRedirect = () => {
+        if(!user){
+            return children
+        }
+
+        return (
+            <Redirect to={{
+                pathname: loggedInPath,
+            }} />
+        );
+    }
+
+    return (
+        <Route
+            {...rest}
+            render={checkUserRedirect}
+        />
+    )
+}
+
+export default IsUserRedirect
