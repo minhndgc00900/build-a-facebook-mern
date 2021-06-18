cf9a0009d018d22e8373ddd32acd44b6c70c356a
Minh Dinh Nguyen <minh.nguyen2@hitachivantara.com>
2021-06-18 19:19:40 +0700

    in progress navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions */
 import React from 'react'
 import {
   BrowserRouter as Router,
+  Redirect,
   // Switch,
   Route
 } from 'react-router-dom'
@@ -11,29 +14,31 @@ import {
   SignIn,
   SignUp
 } from './pages'
-import { IsUserRedirect } from './helpers/routes';
+// import { IsUserRedirect } from './helpers/routes'
 
 function App () {
-  const user = {};
+  const user = {}
 
-  const isUserRedirect = (children, loggedInPath) => {
-    !user ? children : (
+  const isUserRedirect = children => {
+    user
+      ? children
+      : (
       <Redirect to={{
-          pathname: loggedInPath,
+        pathname: ROUTES.SIGN_IN
       }} />
-  )
+        )
   }
 
   return (
     <Router>
-      <Route exact 
-        path={ROUTES.HOME} 
-        component={Home} 
-        render={isUserRedirect(ROUTES.SIGN_IN ,ROUTES.HOME)}
+      <Route exact
+        path={ROUTES.HOME}
+        component={Home}
+        render={isUserRedirect(ROUTES.SIGN_IN, ROUTES.HOME)}
       />
-      <Route 
-        path={ROUTES.SIGN_IN} 
-        component={SignIn}  />
+      <Route
+        path={ROUTES.SIGN_IN}
+        component={SignIn} />
       <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
       <Route exact path={ROUTES.BROWSE} component={Browse} />
     </Router>
